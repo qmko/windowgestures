0346e0315e11c9119b974c4c667cc05f2d6659b6
thyttan <6uuxstm66@mozmail.com>
2026-02-04 21:52:21 +0100

    remove rebuild.sh script

--- rebuild.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-./build.sh && 
-
-  unzip -u windowgestures@extension.amarullz.com.zip -d windowgestures@extension.amarullz.com &&
-
-  glib-compile-schemas windowgestures@extension.amarullz.com/schemas
